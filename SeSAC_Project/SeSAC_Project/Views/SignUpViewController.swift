b9690ae7766181f075030ef45a0fdeb490f3dece
chansenbbai <chanchu@hansung.ac.kr>
2022-01-03 14:33:11 +0900

    [#1] initial commit
    ### Description
    
    - MVVM 폴더링
    
    Resolve #1

---
+++ SeSAC_Project/SeSAC_Project/Views/SignUpViewController.swift
@@ -0,0 +1,16 @@
+//
+//  SignUpViewController.swift
+//  SeSAC_Project
+//
+//  Created by 김승찬 on 2022/01/03.
+//
+
+import UIKit
+
+class SignUpViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .yellow
+    }
+}
